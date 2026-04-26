f35d2a6408cfb6fadddd5b5dcaa446682d57ae61
Ali127Hub <AliMoradi0Business@gmail.com>
2026-04-26 22:10:04 +0330

    fix: fix SwaggerErrOutput swagger meta example, resolves #1

--- swagger.go
+++ swagger.go
@@ -5,7 +5,7 @@ package xerr
 // This is used only for Swagger and documentation.
 // Actual API responses are produced by MarshalJSON of xerr.Error.
 type SwaggerErrOutput struct {
-	Code    Code           `json:"code" example:"INVALID_ARGUMENT"`
+	Code    string         `json:"code" example:"INVALID_ARGUMENT"`
 	Message string         `json:"message,omitempty" example:"invalid request body"`
-	Meta    map[string]any `json:"meta,omitempty" example:"field=username"`
+	Meta    map[string]any `json:"meta,omitempty" swaggertype:"object,string" example:"field:username,error:invalid_length"`
 } // @name Error
